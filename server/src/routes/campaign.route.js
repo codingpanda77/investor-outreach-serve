fde6da53d5f4c90023b17891f38e32f759783d52
codingpanda77 <codingpanda77@gmail.com>
2025-06-20 13:36:32 +0530

    Update campaign.route.js

--- server/src/routes/campaign.route.js
+++ server/src/routes/campaign.route.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 // const { verifyJWT } = require("../middlewares/auth.middleware");
 
-const { createCampaign } = require("../controllers/campaign.controller");
+const {
+  createCampaign,
+  getCampaignReport,
+} = require("../controllers/campaign.controller");
 
 // router.use(verifyJWT);
 
 router.route("/").post(createCampaign);
+router.route("/:campaignId").get(getCampaignReport);
 
 module.exports = router;
