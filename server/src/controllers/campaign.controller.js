0968638e87af34c10388798f9b139c884ae823e4
codingpanda77 <codingpanda77@gmail.com>
2025-06-20 13:35:03 +0530

    Update campaign.controller.js

--- server/src/controllers/campaign.controller.js
+++ server/src/controllers/campaign.controller.js
@@ -2,7 +2,7 @@ const Campaign = require("../models/campaign.model");
 
 exports.createCampaign = async (req, res) => {
   try {
-    const { name, type, company_id } = req.body;
+    const { name, company_id } = req.body;
 
     if (!company_id) {
       return res.status(400).json({ message: "company_id are required." });
@@ -10,7 +10,6 @@ exports.createCampaign = async (req, res) => {
 
     const newCampaign = await Campaign.create({
       name,
-      type: type || "regular",
       company_id,
     });
 
@@ -20,5 +19,22 @@ exports.createCampaign = async (req, res) => {
   } catch (error) {
     console.error("Error creating campaign:", error);
     res.status(500).json({ message: "Failed to create campaign" });
+  }
+};
+
+exports.getCampaignReport = async (req, res) => {
+  try {
+    const { campaignId } = req.params;
+
+    const campaign = await Campaign.findById(campaignId).lean();
+    if (!campaign)
+      return res.status(404).json({ message: "Campaign not found" });
+
+    res.json({
+      campaign,
+    });
+  } catch (err) {
+    console.error("Error fetching report:", err);
+    res.status(500).json({ message: "Failed to get campaign report" });
   }
 };
