d16e14cdd8640d9e528845e8649ab3b9dd0c2785
codingpanda77 <codingpanda77@gmail.com>
2025-06-20 13:36:05 +0530

    Update emailStatus.route.js

--- server/src/routes/emailStatus.route.js
+++ server/src/routes/emailStatus.route.js
@@ -8,12 +8,15 @@ const {
   trackOpen,
   updateDeliveryStatus,
   storeReply,
+  getCampaignReport,
+  getEmailCampaignReport,
 } = require("../controllers/emailStatus.controller");
 
 // router.use(verifyJWT);
 
 router.route("/send").post(sendEmail);
 router.route("/receive").post(storeReply);
+router.route("/report/:id").get(getEmailCampaignReport);
 router.route("/track").get(trackOpen).post(updateDeliveryStatus);
 
 module.exports = router;
